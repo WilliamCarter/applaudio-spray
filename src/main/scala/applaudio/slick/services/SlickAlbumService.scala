76ceac7f110739891eab675d93c6704e37562e65
William Carter <williamcarter1988@gmail.com>
2014-12-07 19:38:24 +0000

    Database agnostic SlickService

--- src/main/scala/applaudio/slick/services/SlickAlbumService.scala
+++ src/main/scala/applaudio/slick/services/SlickAlbumService.scala
@@ -4,13 +4,13 @@ import applaudio.models.Album
 import applaudio.services.AlbumService
 import applaudio.slick.tables.{TrackTable, Tracks}
 
-import scala.slick.driver.MySQLDriver.simple._
+class SlickAlbumService extends SlickService with AlbumService {
 
-class SlickAlbumService extends AlbumService {
+  import driver.simple._
 
   val tracks = TableQuery[TrackTable]
 
-  override def byArtist(artist: String): List[Album] = Database.forConfig("db") withSession { implicit session: Session =>
+  override def byArtist(artist: String): List[Album] = withSession { implicit session: Session =>
     Tracks.byArtist(artist).map(_.album).filter(_.isDefined).list.distinct.map {
       album => Album(album.get)
     }
---
+++ src/main/scala/applaudio/slick/services/SlickService.scala
@@ -0,0 +1,21 @@
+package applaudio.slick.services
+
+import com.typesafe.config.ConfigFactory
+
+import scala.slick.driver._
+import scala.slick.jdbc.JdbcBackend._
+
+class SlickService {
+
+  private[this] val DatabaseConfigKey = "db"
+
+  val driver = ConfigFactory.load().getString(s"$DatabaseConfigKey.driver") match {
+    case "com.mysql.jdbc.Driver" => MySQLDriver
+    case _ => H2Driver
+  }
+
+  def withSession[T](f: Session => T) = Database.forConfig(DatabaseConfigKey) withSession { implicit session: Session =>
+    f(session)
+  }
+
+}
