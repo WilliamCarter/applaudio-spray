13e1e645b45e4a64932ecc1ade6ccadbde2601b0
William Carter <williamcarter1988@gmail.com>
2014-12-07 23:02:01 +0000

    Test for artist routes

---
+++ src/test/scala/acceptance/ArtistsSpec.scala
@@ -0,0 +1,32 @@
+package acceptance
+
+import akka.actor.ActorRefFactory
+import applaudio.models.Artist
+import applaudio.routing.ApplaudioRouting
+import org.specs2.mutable.Specification
+import spray.httpx.encoding.Gzip
+import spray.testkit.Specs2RouteTest
+import spray.http.StatusCodes._
+import spray.http.MediaTypes._
+
+
+class ArtistsSpec extends Specification with Specs2RouteTest with ApplaudioRouting {
+  def actorRefFactory: ActorRefFactory = system
+
+  "Requests for artists" should {
+
+    "return a 200 response" in {
+      Get("/api/artists") ~> routes ~> check {
+        status should be (OK)
+      }
+    }
+
+    "return a JSON response" in {
+      Get("/api/artists") ~> routes ~> check {
+        mediaType should be (`application/json`)
+      }
+    }
+
+  }
+
+}
