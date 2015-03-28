9467f36bd449feb65909926b8e298959d3c76f93
William Carter <williamcarter1988@gmail.com>
2015-03-28 15:10:16 +0000

    Serialise Track from/to JSON with track URLs

--- src/main/scala/applaudio/models/Track.scala
+++ src/main/scala/applaudio/models/Track.scala
@@ -1,6 +1,6 @@
 package applaudio.models
 
-import spray.json.DefaultJsonProtocol
+import spray.json._
 
 case class Track (
   id: Option[Long],
@@ -19,5 +19,35 @@ object Track extends DefaultJsonProtocol {
       year: Option[Int], encoding: String): Track =
     apply(None, title, artist, album, albumTrack, length, year, encoding)
 
-  implicit val trackFormat = jsonFormat8(Track.apply)
+  implicit object TrackJsonFormat extends RootJsonFormat[Track] {
+
+    def write(track: Track) = JsObject (
+      ("id", track.id.map(JsNumber(_)).getOrElse(JsNull)),
+      ("title", JsString(track.title)),
+      ("artist", jsStringOrNull(track.artist)),
+      ("album", jsStringOrNull(track.album)),
+      ("albumTrack", jsNumberOrNull(track.albumTrack)),
+      ("length", jsNumberOrNull(track.length)),
+      ("year", jsNumberOrNull(track.year)),
+      ("encoding", JsString(track.encoding)),
+      ("url", jsStringOrNull(track.id.map { id => s"/library/$id.${track.encoding}" })),
+      ("downloadUrl", track.id.map { id => JsString(s"/library/downloads/$id.${track.encoding}") }.getOrElse(JsNull))
+    )
+
+    def read(value: JsValue) = {
+      val id = fromField[Option[Long]](value, "id")
+      val title = fromField[String](value, "title")
+      val artist = fromField[Option[String]](value, "artist")
+      val album = fromField[Option[String]](value, "album")
+      val albumTrack = fromField[Option[Int]](value, "albumTrack")
+      val length = fromField[Option[Int]](value, "length")
+      val year = fromField[Option[Int]](value, "year")
+      val encoding = fromField[String](value, "encoding")
+      Track(id, title, artist, album, albumTrack, length, year, encoding)
+    }
+
+    private def jsStringOrNull(maybeString: Option[String]):JsValue = maybeString.map(JsString.apply).getOrElse(JsNull)
+    private def jsNumberOrNull(maybeNumber: Option[Int]):JsValue = maybeNumber.map(JsNumber.apply).getOrElse(JsNull)
+  }
+
 }
--- src/test/scala/applaudio/models/TrackSpec.scala
+++ src/test/scala/applaudio/models/TrackSpec.scala
@@ -1,13 +1,24 @@
 package applaudio.models
 
-import org.specs2.mutable.Specification
 import org.specs2.matcher._
+import org.specs2.mutable.Specification
 import spray.json._
 
 class TrackSpec extends Specification with JsonMatchers {
 
-  val jamesAndTheColdGun = Track(
-    id = Option(38L),
+  "The Artist Object" can {
+    "serialise to JSON" in {
+      jamesAndTheColdGun(id = Some(38L)).toJson must be equalTo jamesAndTheColdGunJson(id = "38")
+    }
+    "serialise from JSON with null id" in {
+      val track = Track.TrackJsonFormat.read(jamesAndTheColdGunJson(id = "null"))
+      track must be equalTo jamesAndTheColdGun(id = None)
+    }
+   }
+
+
+  def jamesAndTheColdGun(id: Option[Long]) = Track(
+    id = id,
     title = "James and the Cold Gun",
     artist = Option("Kate Bush"),
     album = Option("The Kick Inside"),
@@ -17,24 +28,20 @@ class TrackSpec extends Specification with JsonMatchers {
     encoding = "mp3"
   )
 
-  val jamesAndTheColdGunJson =
-    """
+  def jamesAndTheColdGunJson(id: String) =
+    s"""
       |{
-      |    "id": 38,
+      |    "id": $id,
       |    "title": "James and the Cold Gun",
       |    "artist": "Kate Bush",
       |    "album": "The Kick Inside",
       |    "albumTrack": 7,
       |    "length": 217,
       |    "year": 1978,
-      |    "encoding": "mp3"
+      |    "encoding": "mp3",
+      |    "url": "/library/$id.mp3",
+      |    "downloadUrl": "/library/downloads/$id.mp3"
       |}
     """.stripMargin.parseJson
 
-  "The Artist Object" can {
-     "serialise to JSON" in {
-       jamesAndTheColdGun.toJson must be equalTo jamesAndTheColdGunJson
-     }
-   }
-
- }
+}
