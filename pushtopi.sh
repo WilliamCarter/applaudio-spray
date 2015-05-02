cd4b76253d4460974b784cc3553de4dd38163741
William Carter <williamcarter1988@gmail.com>
2015-05-02 19:41:23 +0100

    Add server files to gitignore

--- pushtopi.sh
+++ pushtopi.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+pipaddress="pi@192.168.1.96"
+
+function copytopi {
+    scp target/scala-*/Applaudio-*.jar "$pipaddress:applaudio/"
+    scp -r server "$pipaddress:applaudio/server"
+}
 
 sbt clean test acceptance:test grunt:deploy assembly &&
-scp target/scala-*/Applaudio-*.jar pi@192.168.1.96:applaudio/
+copytopi
